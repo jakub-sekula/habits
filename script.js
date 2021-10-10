d043e714b3227eeb21095b29a6bdbfe07829c637
Jakub Sekula <seklerek@gmail.com>
2021-10-10 21:16:50 +0100

    matched layout and style to figma mockup. added animations for all bars

--- script.js
+++ script.js
@@ -10,10 +10,17 @@ window.onload = updateDate;
 
 
 function setRandomWidth(){
-	let randomPercentage = 100 * Math.random() + 5;
-	if (randomPercentage > 100) {
-		randomPercentage = 100;
+	let bars = ["bar1","bar2","bar3","bar4","bar5","bar6","bar7","bar8","bar9","bar10"]
+	
+
+	for (let i = 0; i<bars.length; i++){
+		let randomPercentage = 100 * Math.random() + 5;
+
+		if (randomPercentage > 100) {
+			randomPercentage = 100;
+		}
+
+		document.getElementById(bars[i]).style.width=randomPercentage+"%";
 	}
-	document.getElementById("kappa").style.width=randomPercentage+"%";
 }
 setInterval(setRandomWidth,2500);
