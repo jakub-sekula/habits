61b75a9d5d825b25a2c647149e866fd30620bc38
Jakub Sekula <seklerek@gmail.com>
2023-03-14 22:52:39 +0000

    Initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,9 @@
+import app from ".";
+
+const port = process.env.PORT || "3000";
+
+app.listen(port, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:3000
+⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`)
+);
